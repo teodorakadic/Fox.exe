0e3de952728708fb247728b007d93a03ba5d00cb
thea <teodorakadic@gmail.com>
2021-08-01 10:13:44 +0200

    Initial commit

---
+++ fox_project/Assets/Fox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour {
+
+    private Animator anim;
+    private CharacterController controller;
+
+    public float speed = 6.0f;
+    public float turnSpeed = 60.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    public float gravity = 20.0f;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        anim = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("up") || Input.GetKey("left") || Input.GetKey("right"))
+        {
+            anim.SetInteger("AnimPar", 1);
+        }
+        else
+        {
+            anim.SetInteger("AnimPar", 0);
+        }
+
+        if (controller.isGrounded)
+        {
+            moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+        }
+
+        float turn = Input.GetAxis("Horizontal");
+        transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection.y -= gravity * Time.deltaTime;
+    }
+
+}
